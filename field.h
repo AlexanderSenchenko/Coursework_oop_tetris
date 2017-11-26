b2cb82cabe65159396b90b24bb25e1928c9a62e5
Antihayp <zayzsacha@mail.ru>
2017-11-26 21:57:46 +0700

    Save elem

--- field.h
+++ field.h
@@ -1,8 +1,15 @@
 #ifndef FIELD_H
 #define FIELD_H
 
-const unsigned int coord_y_scr = 0;
+#include "field.h"
+#include <string>
+
+using namespace std;
+
+const unsigned int coord_y_scr = 3;
 const unsigned int coord_x_scr = 25;
+const int start_y_obj = 1;
+const int start_x_obj = 10;
 
 class Field
 {
@@ -14,9 +21,10 @@ class Field
 public:
 	Field();
 	//~Field();
-	int check_x_left(int);
-	int check_x_right(int);
-	
+	int check_x_left();
+	int check_x_right();
+	int check_y_down(WINDOW*);
+
 	int get_line_win();
 	int get_column_win();
 	int get_index(int);
@@ -28,14 +36,19 @@ public:
 
 	void print_obj(WINDOW*);
 	void print_del_obj(WINDOW*);
-
 	void print_info(int);
+	void print_field(WINDOW* win);
+	void print_window_matr();
+
+	void get_figure();
+
+
 };
 
 Field::Field()
 {
-	y_obj = 1;
-	x_obj = 10;
+	y_obj = start_y_obj;
+	x_obj = start_x_obj;
 
 	for (unsigned int i = 0; i < line; i++)
 	{
@@ -56,20 +69,39 @@ Field::Field()
 			field[i][j] = ' ';
 		}
 	}
+
+	field[x_obj][y_obj] = '$';
 }
 
-int Field::check_x_right(int coord)
+int Field::check_x_right()
 {
-	if (coord == line - 2)
-		return coord;
-	return coord + 1;
+	if (x_obj == line - 2)
+		return x_obj;
+	//field[x_obj][y_obj] = ' ';
+	//field[x_obj + 1][y_obj] = '$';
+	return x_obj + 1;
 }
 
-int Field::check_x_left(int coord)
+int Field::check_x_left()
 {
-	if (coord == 1)
-		return coord;
-	return coord - 1;
+	if (x_obj == 1)
+		return x_obj;
+	//field[x_obj][y_obj] = ' ';
+	//field[x_obj - 1][y_obj] = '$';
+	return x_obj - 1;
+}
+
+int Field::check_y_down(WINDOW* win)
+{
+	if (y_obj == line - 2 && field[x_obj][y_obj + 1] != ' ') {
+		field[start_x_obj][start_y_obj] = ' ';
+		field[x_obj][y_obj] = '$';
+		y_obj = start_y_obj;
+		x_obj = start_x_obj;
+		print_field(win);
+		return y_obj;
+	}
+	return y_obj + 1;
 }
 
 int Field::get_line_win() {return line;}
@@ -84,13 +116,13 @@ int Field::get_index(int num_fig)
 void Field::key_left(WINDOW* win)
 {
 	print_del_obj(win);
-	x_obj = check_x_left(x_obj);
+	x_obj = check_x_left();
 }
 
 void Field::key_right(WINDOW* win)
 {
 	print_del_obj(win);
-	x_obj = check_x_right(x_obj);
+	x_obj = check_x_right();
 }
 
 void Field::key_up(WINDOW* win)
@@ -102,7 +134,7 @@ void Field::key_up(WINDOW* win)
 void Field::key_down(WINDOW* win)
 {
 	print_del_obj(win);
-	y_obj++;
+	y_obj = check_y_down(win);
 }
 
 void Field::print_obj(WINDOW* win)
@@ -119,6 +151,36 @@ void Field::print_info(int key)
 {
 	mvprintw(0, 0, "Key: %c %d", key, key);
 	mvprintw(1, 0, "y, x: %d %d", y_obj, x_obj);
+}
+/*
+void Field::get_figure()
+{
+	int *obj = figure.get_obj(), num = 0, x, y;
+
+	num = get_index(0);
+	for (int i = 0; i < 4; i++, num += 2)
+	{
+		x = obj[num] + 1;
+		y = obj[num + 1] + 10;
+		arr_point[i].set_point(x, y);
+		field[x][y] = '$';
+	}
+	//print_field();
+}*/
+
+void Field::print_field(WINDOW* win)
+{
+	unsigned int y_matr = coord_y_scr, x_matr = 0;
+	string test;
+	for (unsigned int i = 1; i < line - 1; i++)
+	{
+		for (unsigned int j = 1; j < column - 1; j++)
+		{
+			mvprintw(j + y_matr, i + x_matr, "%c ", field[i][j]);
+			test = field[i][j];
+			mvwprintw(win, j, i, &test[0]);
+		}
+	}
 }
 
 #endif
--- game.h
+++ game.h
@@ -2,20 +2,28 @@
 #define GAME_H
 
 #include <iostream>
+#include <unistd.h>
 #include <ncurses.h>
+#include <stdlib.h>
+#include <ctime> 
+//#include <sys/time.h>
 #include "field.h"
+
+using namespace std;
 
 class Game
 {
 	Field field;
 	int key;
 	WINDOW *win;
+	static const int time = 300000;
 public:
 	Game();
 	~Game();
 	void start_game();
 	void render_window();
 	void key_processing(int);
+	double wtime();
 };
 
 Game::Game()
@@ -25,9 +33,12 @@ Game::Game()
 	cbreak();
 	keypad(stdscr, true);
 	noecho();
+	nodelay(stdscr, true);
 
 	win = newwin(field.get_line_win(), field.get_column_win(), coord_y_scr, coord_x_scr);
 	refresh();
+	//mvwprintw(win, 1, 1, "%d", time);
+	field.print_field(win);
 	render_window();
 }
 
@@ -44,10 +55,22 @@ void Game::render_window()
 	wrefresh(win);
 }
 
+/*double Game::wtime()
+{
+	struct timeval t;
+	gettimeofday(&t, NULL);
+	return (double)t.tv_sec + (double)t.tv_usec * 1E-6;
+}*/
+
 void Game::start_game()
 {
+	int time_down = clock();
 	while ((key = getch()) != 27) {
-		
+		if ((clock() - time_down) >= (float)time) {
+			time_down = clock();
+			field.key_down(win);
+		}
+
 		key_processing(key);
 
 		render_window();
@@ -61,10 +84,10 @@ void Game::key_processing(int key)
 	switch(key)
 	{
 		case KEY_UP:
-			field.key_up(win);
+			//field.key_up(win);
 			break;
 		case KEY_DOWN:
-			field.key_down(win);
+			//field.key_down(win);
 			break;
 		case KEY_RIGHT:
 			field.key_right(win);
